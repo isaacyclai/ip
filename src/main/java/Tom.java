eaed5fbdea6feebe86272f3de44ecaa71b69dc4f
isaacyclai <isaac.yc.lai@gmail.com>
2025-08-22 00:32:55 +0800

    Rename, Greet, Exit

--- src/main/java/Tom.java
+++ src/main/java/Tom.java
@@ -1,10 +1,16 @@
-public class Duke {
+public class Tom {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " _____ ____  __  __ \n" +
+//                "|_   _/ __ \\|  \\/  |\n" +
+//                "  | || |  | | \\  / |\n" +
+//                "  | || |__| | |\\/| |\n" +
+//                "  |_| \\____/|_|  |_|";
+//        System.out.println("Hello from\n" + logo);
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        System.out.println("Hello! I'm Tom.\nWhat can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
