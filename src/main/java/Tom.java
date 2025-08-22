f1e5cd15f6cef0f6b91e2b5cb43c6717ed83857f
isaacyclai <isaac.yc.lai@gmail.com>
2025-08-22 11:30:35 +0800

    Mark as Done

--- src/main/java/Tom.java
+++ src/main/java/Tom.java
@@ -10,7 +10,7 @@ public class Tom {
 //        System.out.println("Hello from\n" + logo);
 //        String line = "____________________________________________________________";
         System.out.println("Hello! I'm Tom.\nWhat can I do for you?");
-        ArrayList<String> ls = new ArrayList<>();
+        ArrayList<Task> ls = new ArrayList<>();
         while(true) {
             Scanner sc = new Scanner(System.in);
             String input = sc.nextLine();
@@ -18,13 +18,29 @@ public class Tom {
                 break;
             }
             else if(input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for(int i = 0; i < ls.size(); i++) {
-                    System.out.println((i+1) + ". " + ls.get(i));
+                    System.out.println((i+1) + ". " + "[" + ls.get(i).getStatusIcon() + "] "+ ls.get(i).description);
                 }
             }
             else {
-                ls.add(input);
-                System.out.println("added: " + input);
+                String[] strings = input.split(" ");
+                if(strings.length == 2 && strings[0].equals("mark") && Character.isDigit(strings[1].charAt(0))) {
+                    Task cur = ls.get(Integer.valueOf(strings[1]));
+                    cur.Mark();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  [X] " + cur.description);
+                }
+                else if(strings.length == 2 && strings[0].equals("unmark") && Character.isDigit(strings[1].charAt(0))) {
+                    Task cur = ls.get(Integer.valueOf(strings[1]));
+                    cur.Unmark();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("  [ ] " + cur.description);
+                }
+                else {
+                    ls.add(new Task(input));
+                    System.out.println("added: " + input);
+                }
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
