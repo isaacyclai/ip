7ac395056f171d0f7ac6302205f9aaa08e00daed
isaacyclai <isaac.yc.lai@gmail.com>
2025-10-19 13:30:23 +0800

    Fix deadline test

--- src/test/java/tom/DeadlineTest.java
+++ src/test/java/tom/DeadlineTest.java
@@ -11,8 +11,8 @@ public class DeadlineTest {
     @Test
     public void testDeadline() {
         DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
-        LocalDateTime by = LocalDateTime.parse("2025-09-15 1700", inputFormatter);
-        Deadline dl = new Deadline("submit project proposal", by);
-        assertEquals("D | 0 | submit project proposal | Sep 15 2025, 05:00 pm", dl.saveDesc());
+        LocalDateTime by = LocalDateTime.parse("2025-10-19 1300", inputFormatter);
+        Deadline dl = new Deadline("do something", by);
+        assertEquals("D | 0 | do something | Oct 19 2025, 01:00 pm", dl.saveDesc());
     }
 }
